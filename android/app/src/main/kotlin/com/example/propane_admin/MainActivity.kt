ddaacde55b2fa1dc3dbeb348849348353688c0c6
Devconst-Account <usama@devconst.com>
2025-01-23 01:15:32 +0500

    gh-pages branch created

--- android/app/src/main/kotlin/com/example/propane_admin/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.propane_admin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/view/screens/auth_screen/controller/auth_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import '../../../../utils/app_colors.dart';
-
-
-class AuthController extends GetxController {
-  var isChecked = false.obs;
-}
